21da067524a07df52ef6c5e984c126ab937ddb90
idevz <zhoujing_k49@163.com>
2019-10-30 17:51:55 +0800

    init sys module

---
+++ sys/legoes/helpers.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+### BEGIN ###
+# Author: lego users
+# Since: 17:44:19 19/10/30
+# Description:  function about sys
+# base          source ./helpers.sh
+#
+# Environment variables that control this script:
+#
+#   ██▓    ▓█████   ▄████  ▒█████
+#  ▓██▒    ▓█   ▀  ██▒ ▀█▒▒██▒  ██▒
+#  ▒██░    ▒███   ▒██░▄▄▄░▒██░  ██▒
+#  ▒██░    ▒▓█  ▄ ░▓█  ██▓▒██   ██░
+#  ░██████▒░▒████▒░▒▓███▀▒░ ████▓▒░
+#  ░ ▒░▓  ░░░ ▒░ ░ ░▒   ▒ ░ ▒░▒░▒░
+#  ░ ░ ▒  ░ ░ ░  ░  ░   ░   ░ ▒ ▒░
+#    ░ ░      ░   ░ ░   ░ ░ ░ ░ ▒
+#      ░  ░   ░  ░      ░     ░ ░
+#
+### END ###
+
+set -e
+
+LEGO_ROOT=$(dirname $(cd $(dirname "$0") && pwd -P)/$(basename "$0"))
+COMMON_LEGO_ROOT=${LEGO_ROOT}/lego/legoes/
+
+function your_function() {
+    echo "do something."
+}
